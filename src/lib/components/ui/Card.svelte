81c05c6fd03bd2d5615bacc18aede38f5f9948e6
Uncle Common <krishnerkar@gmail.com>
2025-05-22 22:16:51 +0000

    Generated website updates for version: 588

--- src/lib/components/ui/Card.svelte
+++ src/lib/components/ui/Card.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <article
-	class="bg-card hover:bg-sidebar-primary-foreground hover:text-primary-900 border-border grid content-between gap-16 rounded-(--radius) border p-4 px-5 text-pretty shadow-[0_4px_24px_0_rgba(188,144,84,0.10)] transition duration-300 ease-out lg:p-5 lg:px-6 {customClass}"
+	class="bg-white/50 grid content-between gap-16 rounded-(--radius) p-4 px-5 text-pretty shadow-[0_4px_24px_0_rgba(188,144,84,0.10)] transition duration-300 ease-out lg:p-5 lg:px-6 {customClass}"
 >
 	<h3 class="text-title3 mb-2">
 		{title}
